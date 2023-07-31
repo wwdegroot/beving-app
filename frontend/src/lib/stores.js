22a166b6cc61b118382437251a543dfb60a3b93b
wwdegroot <wouter.w.de.groot@gmail.com>
2023-07-31 11:39:19 +0200

    stores for map and view object

--- frontend/src/lib/stores.js
+++ frontend/src/lib/stores.js
@@ -1,5 +1,8 @@
 import { writable } from "svelte/store";
 import Map  from 'ol/Map';
+import View from 'ol/View';
+
+export const viewstore = writable(new View())
 
 
 export const mapstore = writable({
