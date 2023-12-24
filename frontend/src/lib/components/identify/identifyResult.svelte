c2c0e0f918789711593dbf1ff9364e5db1146ce2
wwdegroot <wouter.w.de.groot@gmail.com>
2023-12-24 11:34:15 +0100

    Identify via extent icm schaal. Eerste opzet Resultaat scherm

---
+++ frontend/src/lib/components/identify/identifyResult.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { FeatureLike } from 'ol/Feature';
+
+	export let feature: FeatureLike;
+	const { date, year, depth, mag, place, time, ...rest } = feature.getProperties();
+</script>
+
+<div class="bg-white grid grid-rows-3">
+	<div><p class="font-bold text-xl">{place} - {date} {time}</p></div>
+	<div><span>Magnitude</span><span>{mag}</span></div>
+	<div><span>Diepte</span><span>{depth}</span></div>
+</div>
